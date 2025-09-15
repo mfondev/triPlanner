27e85cddb187bc48c15d9083b7679ac575695cfa
Gideon Atauba <ataubagideon@gmail.com>
2025-09-15 21:42:43 +1300

    feat: add index redirect to login and simplify layout screen options

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
-
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
 export default function _layout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="login" />
     </Stack>
   )
 }
