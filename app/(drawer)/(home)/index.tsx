dede6e1b92433e9f0902639a8d344ba1ee8861bc
Gideon Atauba <ataubagideon@gmail.com>
2025-09-17 02:12:00 +1300

    feat: add Category component with image categories and integrate into home screen

--- app/(drawer)/(home)/index.tsx
+++ app/(drawer)/(home)/index.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/theme'
+import Category from '@/components/h0me/category'
 
 export default function index() {
   return (
     <View style={styles.container}>
       <Text>Let's Travel</Text>
+      <Category />
+      
     </View>
   )
 }
